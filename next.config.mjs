95e69af0a7b55719445271e008d50f5fa3308798
nasaee <nasaee.dev@gmail.com>
2024-09-22 00:07:23 +0700

    implement product review

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'hwtxmvjfrteubzmcslid.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
     ],
   },
 };
